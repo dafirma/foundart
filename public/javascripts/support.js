db553597aff39291f3fe85eb7e83a3d357d84a33
Hugo Resende <dafirma@gmail.com>
2019-03-18 00:22:16 +0100

    add button back white and hide-show search button

--- public/javascripts/support.js
+++ public/javascripts/support.js
@@ -8,6 +8,22 @@ const hideElement = (clicked, hidde) => { //Type string
 hideElement('.menu-item', '#menu-content'); // Display menu
 hideElement('#btn-update-password', '#user-update-password');
 
+// Hide / Show button search
+function openForm() {
+  document.getElementById('main-search').style.display = 'block';
+  let btn = document.getElementById('container-btn-search');
+  if (btn.style.display === 'block') {
+    btn.style.display = 'none';
+  } else {
+    btn.style.display = 'block';
+  }
+  document.getElementById('container-btn-search').style.display = 'none';
+}
+
+function closeForm() {
+  document.getElementsById('main-search').style.display = 'none';
+}
+
 // Flash msg
 if ($('.alert').length > 0) {
   setTimeout(() => {
