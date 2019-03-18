09e6282214b5288db024cfe804fc3b06b43514de
monica <monica>
2019-03-18 22:13:55 +0100

    update search form position

--- public/javascripts/support.js
+++ public/javascripts/support.js
@@ -7,22 +7,23 @@ const hideElement = (clicked, hidde) => { //Type string
 }
 hideElement('.menu-item', '#menu-content'); // Display menu
 hideElement('#btn-update-password', '#user-update-password');
+hideElement('#open-search', '#main-search'); // Display serach form
 
 // Hide / Show button search
-function openForm() {
-  document.getElementById('main-search').style.display = 'block';
-  let btn = document.getElementById('container-btn-search');
-  if (btn.style.display === 'block') {
-    btn.style.display = 'none';
-  } else {
-    btn.style.display = 'block';
-  }
-  document.getElementById('container-btn-search').style.display = 'none';
-}
+// function openForm() {
+//   document.getElementById('main-search').style.display = 'block';
+//   let btn = document.getElementById('container-btn-search');
+//   if (btn.style.display === 'block') {
+//     btn.style.display = 'none';
+//   } else {
+//     btn.style.display = 'block';
+//   }
+//   document.getElementById('container-btn-search').style.display = 'none';
+// }
 
-function closeForm() {
-  document.getElementsById('main-search').style.display = 'none';
-}
+// function closeForm() {
+//   document.getElementsById('main-search').style.display = 'none';
+// }
 
 // Flash msg
 if ($('.alert').length > 0) {
